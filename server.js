55db07bb27bc34ee2200c29a6868f5774d985d20
susana- <susrest3@gmail.com>
2016-03-05 12:03:35 -0500

    Working on the back-end (PT. 1)
    So I made several btnPressed fxns that are called when btns are pressed

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
+var eventful = require('eventful-node');
+var client = new eventful.Client("rcrS54Sn7QXHGhgq");
+var loc = "90210";
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -13,5 +15,66 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
+	console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
+
+app.get('/api/:category/:zipcode', function(req, res){
+	var category = req.params.category;
+	var zipcode = req.params.zipcode;
+	res.send("Music " +  category + " " + zipcode);
+})
+
+app.get('/api/:category/:zipcode', function(req, res){
+	var category = req.params.category;
+	var zipcode = req.params.zipcode;
+	res.send("Comedy " + category + zipcode);
+})
+
+app.get('/api/:category/:zipcode', function(req, res){
+	var category = req.params.category;
+	var zipcode = req.params.zipcode;
+	res.send("Night life " + category + zipcode);
+})
+
+
+
+function btnMusicPressed () {
+	client.searchEvents({ 
+		keywords: 'music' , location: loc, date:'This Week'}, function(err,data){
+			if(err){
+				return console.error(err);
+			}
+
+			console.log('Received ' + data.search.total_items + ' events');
+			console.log('Event listings: ', data.search.events);
+
+		});
+}
+
+function btnComedyPressed () {
+	client.searchEvents({ 
+		keywords: 'comedy' , location: loc, date:'This Week'}, function(err,data){
+			if(err){
+				return console.error(err);
+			}
+
+			console.log('Received ' + data.search.total_items + ' events');
+			console.log('Event listings: ');
+
+		});
+}
+
+function btnNightlifePressed () {
+	client.searchEvents({ 
+		keywords: 'nightlife' , location: loc, date:'This Week'}, function(err,data){
+			if(err){
+				return console.error(err);
+			}
+
+			console.log('Received ' + data.search.total_items + ' events');
+			console.log('Event listings: ');
+
+		});
+}
+
+btnMusicPressed();
